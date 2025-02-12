d4cc30ff8bad766fc1feca30ab19f3e1f3588e61
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-02-12 12:58:14 +0530

    Darker text color

--- app/page.tsx
+++ app/page.tsx
@@ -46,7 +46,7 @@ const Home: React.FC = () => {
           initial={{ opacity: 0, translateY: 10, filter: "blur(10px)" }}
           animate={{ opacity: 1, translateY: 0, filter: "blur(0)" }}
           transition={{ delay: 0.1, type: "spring" }}
-          className="text-xl md:text-2xl font-semibold tracking-tighter text-stone-500 mb-12"
+          className="text-xl md:text-2xl font-semibold tracking-tighter text-stone-700 mb-12"
         >
           <span className="flex mb-6">Hi, I’m Rajarshi Gupta.</span>
           {COPY.map((copy, index) =>
@@ -81,7 +81,7 @@ const Home: React.FC = () => {
             href="https://cal.com/rajarshi/15min"
             target="_blank"
             className={classNames(
-              "flex items-center gap-1 text-stone-500 hover:text-stone-600 focus:text-stone-600 transition-colors relative outline-hidden",
+              "flex items-center gap-1 text-stone-700 hover:text-stone-900 focus:text-stone-900 transition-colors relative outline-hidden",
               "focus:after:w-full hover:after:w-full after:duration-500 after:transition-all after:absolute after:w-0 after:bg-stone-600 after:h-0.5 after:-bottom-0.5 after:left-0"
             )}
           >
@@ -92,7 +92,7 @@ const Home: React.FC = () => {
             href="https://x.com/rajarshikgupta"
             target="_blank"
             className={classNames(
-              "flex items-center gap-1 ml-auto text-stone-500 hover:text-stone-600 focus:text-stone-600 transition-colors relative outline-hidden",
+              "flex items-center gap-1 ml-auto text-stone-700 hover:text-stone-900 focus:text-stone-900 transition-colors relative outline-hidden",
               "focus:after:w-full hover:after:w-full after:duration-500 after:transition-all after:absolute after:w-0 after:bg-stone-600 after:h-0.5 after:-bottom-0.5 after:left-0"
             )}
           >
@@ -103,7 +103,7 @@ const Home: React.FC = () => {
             href="https://www.linkedin.com/in/rajarshikgupta/"
             target="_blank"
             className={classNames(
-              "flex items-center gap-1 text-stone-500 hover:text-stone-600 focus:text-stone-600 transition-colors relative outline-hidden",
+              "flex items-center gap-1 text-stone-700 hover:text-stone-900 focus:text-stone-900 transition-colors relative outline-hidden",
               "focus:after:w-full hover:after:w-full after:duration-500 after:transition-all after:absolute after:w-0 after:bg-stone-600 after:h-0.5 after:-bottom-0.5 after:left-0"
             )}
           >
